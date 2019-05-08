7ebfe3dca61f96acf19a654e942090cea76b1adf
Armando Barron Garcia <madaraushiha54@gmail.com>
2019-05-07 22:49:28 -0500

    A comenzar

---
+++ src/retos2/Retos_2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package retos2;
+
+/**
+ *
+ * @author obito
+ */
+public class Retos_2 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
