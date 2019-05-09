3708595b2ab097e5f7e90d9429eddf4bf93853a5
Armando Barron Garcia <madaraushiha54@gmail.com>
2019-05-08 20:13:54 -0500

    matriz de distancias lista

--- src/retos2/numerosAleatorios.java
+++ src/retos2/numerosAleatorios.java
@@ -45,4 +45,57 @@ public class numerosAleatorios {
         }
         return numerosAleatorios;
     }
+    
+    public int[][] generar_Distancias(int semilla ,int cantidad)
+    {
+        
+        int[][] matriz_Distancias = new int[cantidad][cantidad];
+        String snumero2, snumero3, valor;
+        int tam1, tam2, i, primerc;
+        long numero1, numero2;
+        
+
+        valor = "" + semilla;
+        tam1 = valor.length();
+        System.out.println("Cantidad de digitos:  " + tam1);
+        numero1 = semilla;
+        
+        for (i = 0; i < cantidad; i++) {
+            for (int j = 0; j < cantidad; j++) {
+                if(i==j)
+                {
+                    matriz_Distancias[i][j] = 0;
+                }else
+                {
+                    numero2 = (long) Math.pow(numero1, 2);
+            snumero2 = Long.toString(numero2);
+            tam2 = snumero2.length();
+            primerc = (tam2 - tam1) / 2;
+            snumero3 = snumero2.substring(primerc, primerc + tam1);
+            System.out.println(i + "- " + snumero3);
+            matriz_Distancias[i][j] = Integer.parseInt(snumero3);
+            numero1 = Integer.parseInt(snumero3);
+            //(formateador.format (valores));
+                }
+            
+            }
+            
+        }
+        imprimir_matriz(matriz_Distancias,cantidad);
+        return matriz_Distancias;
+    }
+    
+    
+    
+    public void imprimir_matriz(int[][] matriz, int cantidad)
+    {
+        String imprime="";
+        for (int i = 0; i < cantidad; i++) {
+            for (int j = 0; j < cantidad; j++) {
+                imprime+="-"+matriz[i][j];
+            }
+            imprime+="\n";
+        }
+        System.out.println(imprime);
+    }
 }
